fee5e3a105e23c16d8a8fab249dd4fcf63c4c79e
Ilya <dota2yashicmoy@mail.ru>
2022-12-02 11:32:55 +0300

    Удалено лишнее

--- VKLeadForm/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace VKLeadForm.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- VKLeadForm/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace VKLeadForm.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
